88920d94680b2f8f3eb7e02fd9ec4c00a295c35a
nikolamilanovic21 <nikolamilanovic21@gmail.com>
2020-05-02 18:52:35 +0200

    Added Login and Register

---
+++ backend/microservices/auth/db.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+const isDocker = require('is-docker');
+let connection;
+
+module.exports = function(username, password, server, dbName) {
+   return new Promise((resolve, reject) => {
+      if (connection)
+         resolve(connection)
+
+      const uri = isDocker() ? `mongodb://db/${dbName}` : `mongodb://${username}:${encodeURIComponent(password)}@${server}/${dbName}`;
+
+      MongoClient.connect(uri, (err, db) => {
+         if (err)
+            reject(err)
+         
+         connection = db.db(dbName);
+         resolve(connection) 
+      })
+   })
+}
