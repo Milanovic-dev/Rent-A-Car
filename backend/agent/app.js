d7234f7c58b3ad9ab66f619f292c36edd9345e7f
nikolamilanovic21 <nikolamilanovic21@gmail.com>
2020-05-04 17:43:28 +0200

    Soap Services

--- backend/agent/app.js
+++ backend/agent/app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const soap = require('soap');
 
 app.use(bodyParser.json());
+app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
 app.use(cors());
 
-app.listen(8282, () => {
+const server = http.createServer(app);
+
+server.listen(8282, () => {
     console.log("Agent running on port 8282");
+});
+
+// Soap server
+const service = {
+    MyService: {
+        MyPort: {
+            MyFunction: function(args) {
+                return {
+                    name: args.name
+                };
+            }
+        }
+    }
+};
+const xml = require('fs').readFileSync('service.wsdl', 'utf8');
+
+soap.listen(server, '/wsdl', service, xml, function(){
+    console.log("Soap services activated");
 });
 
 app.get('/', (req, res) => {
