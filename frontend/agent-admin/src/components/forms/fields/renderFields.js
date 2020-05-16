f18b05d6aa2b2e63a6541353e6f1eb101de6b51f
Milan Stanojevic <stanojevic.milan97@outlook.com>
2020-05-16 23:37:10 +0200

    AgentCarListAndCreatingNewCars

---
+++ frontend/agent-admin/src/components/forms/fields/renderFields.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import Text from './text';
+import Image from './image';
+import Select from './select';
+import Check from './check';
+import DateTime from './date_picker';
+import Option from './2letters_option';
+import MultiSelect from './multiSelect';
+import Html from './html';
+import Textarea from './textarea';
+
+
+  export const renderMultiSelectField = ({
+    input,
+    placeholder,
+    label,
+    meta: { touched, error },
+    children,
+    id
+    }) => (
+          <MultiSelect
+              placeholder={placeholder}
+              label={label}
+              id={id}
+              errorText={touched && error}
+              error={touched && error}
+              {...input}
+          >
+              {children}
+          </MultiSelect>
+  )
+
+
+  export const renderTextField = ({
+    input,
+    placeholder,
+    label,
+    type,
+    meta: { touched, error }
+    }) => (
+      <Text
+      placeholder={placeholder}
+      label={label}
+      type={type}
+      errorText={touched && error}
+      error={touched && error}
+      {...input}
+      />
+    )
+
+    export const renderTextareaField = ({
+        input,
+        placeholder,
+        label,
+        type,
+        meta: { touched, error }
+        }) => (
+          <Textarea
+          placeholder={placeholder}
+          label={label}
+          type={type}
+          errorText={touched && error}
+          error={touched && error}
+          {...input}
+          />
+        )
+    
+    export const renderHtmlField = ({
+        input,
+        placeholder,
+        label,
+        type,
+        meta: { touched, error }
+        }) => (
+          <Html
+          placeholder={placeholder}
+          label={label}
+          type={type}
+          errorText={touched && error}
+          error={touched && error}
+          {...input}
+          />
+        )
+    
+  export const renderImageField = ({
+    input,
+    placeholder,
+    meta: { touched, error }
+    }) => (
+        <Image
+            placeholder={placeholder}
+            errorText={touched && error}
+            error={touched && error}
+            {...input}
+        />
+    )
+
+  export const renderCheckField = ({
+    input,
+    label,
+    meta: { touched, error }
+    }) => (
+          <Check
+              label={label}
+              errorText={touched && error}
+              error={touched && error}
+              {...input}
+          />
+    )
+
+    export const renderSelectField = ({
+    input,
+    placeholder,
+    label,
+    meta: { touched, error },
+    children,
+    id
+    }) => (
+          <Select
+              placeholder={placeholder}
+              label={label}
+              id={id}
+              errorText={touched && error}
+              error={touched && error}
+              {...input}
+          >
+              {children}
+          </Select>
+    )
+
+    export const renderDateTimeField = ({
+      input,
+      placeholder,
+      label,
+      meta: { touched, error },
+      }) => (
+  
+          <DateTime
+              placeholder={placeholder}
+              label={label}
+              errorText={touched && error}
+              error={touched && error}
+  
+              {...input}
+          />
+      )
+
+      
+    export const render2letterOption = ({
+      input,
+      placeholder,
+      label,
+      meta: { touched, error },
+      }) => (
+  
+          <Option
+              placeholder={placeholder}
+              label={label}
+              errorText={touched && error}
+              error={touched && error}
+  
+              {...input}
+          />
+      )
+
