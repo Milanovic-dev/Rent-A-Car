af090ece512e2bf34bc0c5781f935e05848f6715
acodabic <acodabic1997@gmail.com>
2020-06-01 13:21:19 +0200

    frontend

---
+++ frontend/agent-site/src/components/forms/carForm.js
@@ -0,0 +1,314 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
+
+
+import {
+    Container,
+    Row,
+    Col,
+} from 'reactstrap';
+import Isvg from 'react-inlinesvg';
+
+import Select from './fields/select';
+import Text from './fields/text';
+import Textarea from './fields/textarea';
+import Html from './fields/html';
+import Image from './fields/image';
+
+import RangeSlider from './fields/rangeSlider';
+
+import car_icon from '../../assets/svg/car.svg';
+
+const renderImageField = ({
+    input,
+    placeholder,
+    meta: { touched, error }
+    }) => (
+        <Image
+            placeholder={placeholder}
+            errorText={touched && error}
+            error={touched && error}
+            {...input}
+        />
+    )
+
+const renderHtmlField = ({
+    input,
+    placeholder,
+    label,
+    type,
+    meta: { touched, error }
+}) => (
+        <Html
+            placeholder={placeholder}
+            label={label}
+            type={type}
+            errorText={touched && error}
+            error={touched && error}
+            {...input}
+        />
+    )
+
+const renderSelectField = ({
+    input,
+    placeholder,
+    label,
+    meta: { touched, error },
+    children,
+}) => (
+
+        <Select
+            placeholder={placeholder}
+            label={label}
+            errorText={touched && error}
+            {...input}
+            children={children}
+        />
+    )
+
+const renderTextField = ({
+    input,
+    placeholder,
+    label,
+    meta: { touched, error },
+}) => (
+
+        <Text
+            placeholder={placeholder}
+            label={label}
+            errorText={touched && error}
+            {...input}
+        />
+    )
+
+const renderTextareaField = ({
+    input,
+    placeholder,
+    label,
+    meta: { touched, error },
+}) => (
+
+        <Textarea
+            placeholder={placeholder}
+            label={label}
+            errorText={touched && error}
+            {...input}
+        />
+    )
+
+const renderRangeSliderField = ({
+    input,
+    label,
+    meta: { touched, error },
+    children,
+    min, max, defaultValue
+}) => (
+
+        <RangeSlider
+            label={label}
+            errorText={touched && error}
+            {...input}
+            children={children}
+            min={min}
+            max={max}
+            defaultValue={defaultValue}
+        />
+    )
+
+
+
+const carForm = (props) => {
+    const { handleSubmit, pristine, reset, submitting } = props;
+    console.log(pristine, submitting);
+
+    return (
+        <form onSubmit={handleSubmit} className="contact-form">
+            <Row>
+                <Col lg="12">
+                    <h3>CAR</h3>
+                </Col>
+                <Col lg="6">
+
+                    <Row>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="make"
+                                component={renderTextField}
+                                // label={"Make"}
+                                placeholder="Make"
+                            ></Field>
+                            {/* <Field 
+                                            type="hidden"
+                                            name="csrf"
+                                            value={{csrftoken}}
+                                        ></Field> */}
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="model"
+                                component={renderTextField}
+                                // label={"Model"}
+                                placeholder="Model"
+                            ></Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="productionYear"
+                                component={renderTextField}
+                                // label={"Production year"}
+                                placeholder="Production year"
+                            ></Field>
+                        </Col>
+
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="fuel"
+                                component={renderSelectField}
+                                // label={"Fuel"}
+                                placeholder="Fuel"
+                            >
+                                <option value="diesel">Diesel</option>
+                                <option value="gasoline">Gasoline</option>
+                                <option value="hybrid">Hybrid</option>
+                                <option value="electric">Electric</option>
+                            </Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="transmission"
+                                component={renderSelectField}
+                                // label={"Transmission"}
+                                placeholder="Transmission"
+                            >
+                                <option value="manual">Manual</option>
+                                <option value="automatic">Automatic</option>
+                            </Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="class"
+                                component={renderSelectField}
+                                // label={"Class"}
+                                placeholder="Class"
+                            >
+                                <option value="cabriolet-roadster">Cabriolet / Roadster</option>
+
+                                <option value="estate-car">Estate Car</option>
+
+                                <option value="saloon">Saloon</option>
+
+                                <option value="small-car">Small Car</option>
+
+                                <option value="sports-car-coupe">Sports Car / Coupe</option>
+
+                                <option value="suv-off-road-vehicle-pickup-truck">SUV / Off-road Vehicle / Pickup Truck</option>
+
+                                <option value="van-minibus">Van / Minibus</option>
+
+
+
+                            </Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="mileage"
+                                component={renderTextField}
+                                // label={"Mileage"}
+                                placeholder="Mileage"
+                            ></Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="limitMileage"
+                                component={renderTextField}
+                                // label={"Limit Mileage"}
+                                placeholder="Limit Mileage"
+                            ></Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="power"
+                                component={renderTextField}
+                                // label={"Power (kW)"}
+                                placeholder="Power (kW)"
+                            ></Field>
+                        </Col>
+
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="color"
+                                component={renderSelectField}
+                                // label={"Color"}
+                                placeholder="Color"
+                            >
+                                <option value="red">Red</option>
+                                <option value="blue">Blue</option>
+                                <option value="black">Black</option>
+                                <option value="silver">Silver</option>
+                                <option value="yellow">Yellow</option>
+                                <option value="green">Green</option>
+                                <option value="white">White</option>
+
+                            </Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="seatCount"
+                                component={renderTextField}
+                                // label={"Seat count"}
+                                placeholder="Seat count"
+                            ></Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="location"
+                                component={renderTextField}
+                                // label={"Location"}
+                                placeholder="Location"
+                            ></Field>
+                        </Col>
+                        <Col lg="6" className="input-wrap">
+                            <Field
+                                name="price"
+                                component={renderTextField}
+                                // label={"Price"}
+                                placeholder="Price"
+                            ></Field>
+                        </Col>
+
+                        <Col lg="12" className="input-wrap">
+                            <Field
+                                name="description"
+                                component={renderHtmlField}
+                                label={"Tekst"}
+                                placeholder=""
+                            ></Field>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col lg="6" className="input-wrap">
+
+                    <Field
+                        name="image"
+                        component={renderImageField}
+
+                    ></Field>
+                </Col>
+
+                <Col lg="12">
+                    <div className="input-wrap buttons">
+                        <button type="submit" className="button" disabled={pristine || submitting}> Save </button>
+                    </div>
+
+                </Col>
+
+            </Row>
+
+        </form>
+
+    )
+}
+
+export default reduxForm({
+    form: 'carForm'  // a unique identifier for this form
+})(carForm)
---
+++ frontend/agent-site/src/views/changeCar.js
@@ -0,0 +1,282 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { PageWithLayout } from '../containers/page';
+import { connect } from 'react-redux';
+
+import Isvg from 'react-inlinesvg';
+import PageHeader from '../containers/header/pageHeader';
+import Footer from '../containers/footer';
+
+import Map from '../components/map';
+import Form from '../components/forms/carForm';
+
+
+import {
+    Container,
+    Row,
+    Col,
+    Carousel,
+    CarouselItem,
+    CarouselControl
+
+} from 'reactstrap';
+// import Article from '../components/article';
+
+
+// import car_image6 from '../assets/images/car6.png';
+// import car_image7 from '../assets/images/car7.png';
+// import car_image8 from '../assets/images/car8.png';
+
+// import fuel_icon from '../assets/svg/fuel.svg';
+// import calendar_icon from '../assets/svg/calendar.svg';
+// import guage_icon from '../assets/svg/guage.svg';
+// import color_icon from '../assets/svg/color.svg';
+// import engine_icon from '../assets/svg/engine.svg';
+// import door_icon from '../assets/svg/car-door.svg';
+// import transmission_icon from '../assets/svg/transmission.svg';
+// import car_icon from '../assets/svg/car-icon.svg';
+
+
+// import gallery1 from '../assets/images/gallery1.png';
+// import gallery2 from '../assets/images/gallery2.png';
+// import gallery3 from '../assets/images/gallery3.png';
+// import gallery4 from '../assets/images/gallery4.png';
+// import gallery5 from '../assets/images/gallery5.png';
+// import gallery6 from '../assets/images/gallery6.png';
+// import gallery7 from '../assets/images/gallery7.png';
+
+// function generateAlias(str) {
+//     /*str = str.toLowerCase();
+//     str = str.replace(/\s\s+/g, ' ');
+//     str = str.replace(/ /g, '-');
+//     str = str.replace(/\./g, '-');
+//     str = str.replace(/\,/g, '-');
+//     str = str.replace(/š/g, 's');
+//     str = str.replace(/č/g, 'c');
+//     str = str.replace(/ć/g, 'c');
+//     str = str.replace(/đ/g, 'dj');
+//     str = str.replace(/ž/g, 'z');*/
+//     str = str.replace(/[^a-zA-Z0-9]/gi, '-').toLowerCase()
+//     return str;
+// }
+
+
+class ChangeCar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onTouchMove = this.onTouchMove.bind(this);
+        this.onTouchEnd = this.onTouchEnd.bind(this);
+        this.onTouchStart = this.onTouchStart.bind(this);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+
+        /*this.next = this.next.bind(this);
+        this.prev = this.prev.bind(this);*/
+
+        this.state = {
+            product: null,
+            previewImage: null,
+            modalIdx: null,
+            products: [],
+            activeIndex: 0,
+            modalIdx: 0,
+            newestProducts: []
+        };
+    }
+
+    onExiting() {
+        this.animating = true;
+    }
+
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === this.state.product.images.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? this.state.product.images.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+        fetch('http://localhost:8282/api/cars/v1/get/' + this.props[0].match.params.id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+        }).then((res) => res.json()).then((result) => {
+            this.setState({
+                product: result,
+                previewImage: result.image
+            })
+        });
+
+
+
+    }
+
+
+    componentDidUpdate(prevProps) {
+        if (prevProps[0].location.pathname != this.props[0].location.pathname) {
+            fetch('https://showroom-api.novamedia.agency/cars/fetch/' + this.props[0].match.params.id).then((res) => res.json()).then((product) => {
+                console.log(product);
+                this.setState({ product, previewImage: (product.images && product.images[0]) });
+
+            })
+
+        }
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+
+    onTouchStart(event) {
+        var x = event.clientX;
+        var y = event.clientY;
+        if (!this.state._startSwipePos) {
+            this.setState({
+                _startSwipePos: x,
+                _startSwipePosY: y,
+                _startLeft: this.carousel.scrollLeft
+            });
+        }
+    }
+
+    onTouchEnd() {
+        this.setState({
+            _startSwipePos: null,
+            _startSwipePosY: null,
+            _startLeft: null
+        });
+    }
+
+    onTouchMove(event) {
+        var x = event.clientX;
+        var y = event.clientY;
+
+        if (this.state._startSwipePos) {
+            this.carousel.scrollLeft = this.state._startLeft - (x - this.state._startSwipePos);
+        }
+
+        this.setState({
+            _swipePos: x
+        });
+
+
+    }
+
+
+    render() {
+        const { activeIndex } = this.state;
+
+        let slides;
+
+        if (this.state.product && this.state.product.images) {
+            slides = this.state.product.images.map((item) => {
+                return (
+                    <CarouselItem
+                        tag="div"
+                        key={item}
+                        onExiting={this.onExiting}
+                        onExited={this.onExited}
+                    >
+                        <div className="lightbox-item">
+                            <img src={'https://showroom-api.novamedia.agency/' + item} />
+                        </div>
+
+                    </CarouselItem>
+                );
+            });
+
+        }
+
+
+        return (
+
+            <div onMouseUp={this.onTouchEnd} className={this.props.menu ? "detail-wrap active-menu-animation" : "detail-wrap"}>
+
+                <PageHeader page='Fahrzeuge' {...this.props} />
+                <div className="page-wrap">
+
+
+                    <Container>
+                        <Row className="page-title">
+                            <Col lg="12">
+                            </Col>
+                        </Row>
+                        {/* {
+                            this.state.data ?
+                                <Form initialValues={this.state.data} onSubmit={this.add} />
+                                :
+                                <Form onSubmit={this.add} />
+
+                        } */}
+                        <Form />
+                    </Container>
+
+
+                    <section className="section map-section">
+                        <Container fluid>
+
+                            <Row>
+                                <Col md="12">
+                                    <Map {...this.props} />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </section>
+                    {/* {slides && this.state.lightbox ?
+                        <div className="lightbox">
+                            <i className="mdi mdi-close" onClick={() => this.setState({ lightbox: null })} />
+                            <Carousel
+                                activeIndex={activeIndex}
+                                next={this.next}
+                                previous={this.previous}
+                                autoPlay={null}
+                            >
+                                {slides}
+                                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                            </Carousel>
+
+                        </div>
+                        : null
+                    } */}
+
+
+                    <Footer {...this.props} />
+
+                </div>
+
+
+
+            </div >
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    menu: state.menu
+});
+
+
+
+export default connect(mapStateToProps)(PageWithLayout(ChangeCar));
