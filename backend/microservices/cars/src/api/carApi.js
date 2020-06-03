f15db385a17911b7802227d79f35c9121e2f825b
milanatucakov <milanatucakov@gmail.com>
2020-06-03 19:19:52 +0200

    Add search and car endpoints

--- backend/microservices/cars/src/api/carApi.js
+++ backend/microservices/cars/src/api/carApi.js
@@ -1,8 +1,8 @@
-const service = require('../service/carService');
+const service = require('../services/carService');
 
 module.exports = function(app){
 
-    app.get('/get/:id', async (req, res) => {
+    app.get('/cars/get/:id', async (req, res) => {
         console.log(req.method + req.route.path);
 
         if(!req.params.id) return res.status('400');
@@ -11,7 +11,7 @@ module.exports = function(app){
         res.status(result.status).send(result.response);
     });
 
-    app.post('/create', async (req, res) => {
+    app.post('/cars/create', async (req, res) => {
         console.log(req.method + req.route.path);
 
         if(!req.body) return res.status('400');
@@ -20,7 +20,7 @@ module.exports = function(app){
         res.status(result.status).send(result.response);
     });
 
-    app.put('/update', async (req, res) => {
+    app.put('/cars/update', async (req, res) => {
         console.log(req.method + req.route.path);
 
         if(!req.body) return res.status('400');
@@ -29,7 +29,7 @@ module.exports = function(app){
         res.status(result.status).send(result.response);
     });
 
-    app.delete('/remove/:id', async (req, res) => {
+    app.delete('/cars/remove/:id', async (req, res) => {
         console.log(req.method + req.route.path);
 
         if(!req.params.id) return res.status('400');
@@ -38,17 +38,17 @@ module.exports = function(app){
         res.status(result.status).send(result.response);
     });
 
-    app.get('/all', async (req, res) => {
+    app.get('/cars/all', async (req, res) => {
         console.log(req.method + req.route.path);
         let result = await service.getAll();
         res.status(result.status).send(result.response);
     });
 
-    app.get('/stats', async (req, res) => {
+    app.get('/cars/stats', async (req, res) => {
 
     });
 
-    app.get('/stats/:id', async (req, res) => {
+    app.get('/cars/stats/:id', async (req, res) => {
 
     });
 };
