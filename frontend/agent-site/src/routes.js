5f6bd49cd5acd6e9b26acfea5e7ed738a2a8e175
acodabic <acodabic1997@gmail.com>
2020-06-10 18:15:40 +0200

    registerForm

--- frontend/agent-site/src/routes.js
+++ frontend/agent-site/src/routes.js
@@ -15,6 +15,10 @@ import InventoryPage from './views/inventoryPage';
 import DetailPage from './views/detailPage';
 import ChangeCar from './views/changeCar';
 import BusyCar from './views/busyCar';
+import Register from './views/register';
+
+
+
 
 
 
@@ -85,6 +89,13 @@ class Routes extends Component {
                             exact
                             render={(...renderProps) => (
                                 <DetailPage {...renderProps} {...this.props} />
+                            )}
+                        />
+                        <Route
+                            path="/register"
+                            exact
+                            render={(...renderProps) => (
+                                <Register {...renderProps} {...this.props} />
                             )}
                         />
 
---
+++ frontend/agent-site/src/views/register.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { PageWithLayout } from '../containers/page';
+import { connect } from 'react-redux';
+
+import Form from '../components/forms/registerForm'
+
+import Isvg from 'react-inlinesvg';
+import PageHeader from '../containers/header/pageHeader';
+import Footer from '../containers/footer';
+
+import Map from '../components/map';
+
+import {
+    Container,
+    Row,
+    Col,
+    Carousel,
+    CarouselItem,
+    CarouselControl
+
+} from 'reactstrap';
+import Article from '../components/article';
+
+
+import car_image6 from '../assets/images/car6.png';
+import car_image7 from '../assets/images/car7.png';
+import car_image8 from '../assets/images/car8.png';
+
+import fuel_icon from '../assets/svg/fuel.svg';
+import calendar_icon from '../assets/svg/calendar.svg';
+import guage_icon from '../assets/svg/guage.svg';
+import color_icon from '../assets/svg/color.svg';
+import engine_icon from '../assets/svg/engine.svg';
+import door_icon from '../assets/svg/car-door.svg';
+import transmission_icon from '../assets/svg/transmission.svg';
+import car_icon from '../assets/svg/car-icon.svg';
+
+
+import gallery1 from '../assets/images/gallery1.png';
+import gallery2 from '../assets/images/gallery2.png';
+import gallery3 from '../assets/images/gallery3.png';
+import gallery4 from '../assets/images/gallery4.png';
+import gallery5 from '../assets/images/gallery5.png';
+import gallery6 from '../assets/images/gallery6.png';
+import gallery7 from '../assets/images/gallery7.png';
+
+function generateAlias(str) {
+    /*str = str.toLowerCase();
+    str = str.replace(/\s\s+/g, ' ');
+    str = str.replace(/ /g, '-');
+    str = str.replace(/\./g, '-');
+    str = str.replace(/\,/g, '-');
+    str = str.replace(/š/g, 's');
+    str = str.replace(/č/g, 'c');
+    str = str.replace(/ć/g, 'c');
+    str = str.replace(/đ/g, 'dj');
+    str = str.replace(/ž/g, 'z');*/
+    str = str.replace(/[^a-zA-Z0-9]/gi, '-').toLowerCase()
+    return str;
+}
+
+
+class DetailPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.submit = this.submit.bind(this);
+        this.state = {
+
+        };
+    }
+
+    submit(data) {
+        console.log(data);
+    }
+
+
+    render() {
+
+
+
+
+        return (
+
+            <div >
+
+                <PageHeader page='Fahrzeuge' {...this.props} />
+                <div className="page-wrap">
+                    <Row>
+                        <Col lg="4">
+                        </Col>
+                        <Col lg="4">
+                            <Form onSubmit={this.submit} />
+                        </Col>
+                        <Col lg="4">
+                        </Col>
+                    </Row>
+
+                </div>
+            </div >
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    menu: state.menu
+});
+
+
+
+export default connect(mapStateToProps)(PageWithLayout(DetailPage));
