2767acae8bdb66cb69e7cfa4cc905c3a6e7b5f3e
nikolamilanovic21 <nikolamilanovic21@gmail.com>
2020-06-10 22:17:13 +0200

    Fix HTTPS Soap

--- backend/agent/src/soap/soapService.js
+++ backend/agent/src/soap/soapService.js
@@ -1,10 +1,20 @@
 const soap = require('soap');
 const fs = require('fs');
+const request = require('request');
 const hostUrl = process.env.HOST_URL + '/webhook/getWsdl';
 const db = require('../../dbSync');
 db.connect();
 
-const options = {};
+const specialRequest = request.defaults({
+    agentOptions: {
+        ca: fs.readFileSync('./src/security/certificates/chain.pem')
+    },
+    strictSSL: false
+})
+
+const options = {
+    request: specialRequest
+};
 
 const service = {
     
@@ -17,14 +27,22 @@ const createService = async (server, callback) => {
 }
 
 const getClient = async () => {
-    const client = await soap.createClientAsync(hostUrl, options);
-    if(db.getDb()){
-        const token = await db.getToken();
-        if(token){
-            client.addSoapHeader(`<AuthToken>${token}</AuthToken>`);
-        }
-    }
-    return client;
+    return new Promise((resolve, reject) => {
+        soap.createClient(hostUrl, options, async (err, client) => {
+            if(err){
+                console.error(err);
+                reject(err);
+            }
+
+            if(db.getDb()){
+                const token = await db.getToken();
+                if(token){
+                    client.addSoapHeader(`<AuthToken>${token}</AuthToken>`);
+                }
+            }
+            resolve(client);
+        });
+    })
 }
 
 module.exports = {
